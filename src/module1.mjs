99cb67514d130cb1345915f0eb785ac3c4765aaa
ahadhan <ahadkhanmay@gmail.com>
2023-08-03 00:21:54 +0500

    Updates

--- src/module1.mjs
+++
@@ -1,9 +0,0 @@
-const a = "Ahad";
-const b = "Khan";
-const c = "Abdul";
-const d = "aka";
-
-export default d;
-export {a};
-export {b};
-export {c};
--- src/module2.mjs
+++
@@ -1,4 +0,0 @@
-import di, {a,b,c} from './module1.mjs'
-console.log(b);
-console.log(a);
-console.log(di);
